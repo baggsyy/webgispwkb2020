0d677d191cf2ebd136ef0a2aa1e903d7a3d7129a
baggsyy <76062394+baggsyy@users.noreply.github.com>
2020-12-15 23:52:53 +0700

    Add files via upload

---
+++ data/bandungtitikacuan_1.js
@@ -0,0 +1,8 @@
+var json_bandungtitikacuan_1 = {
+"type": "FeatureCollection",
+"name": "bandungtitikacuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 107.620001123717628, -6.884964756046954 ] } }
+]
+}
